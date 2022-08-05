71943154b0a89ff941ea2f00aee5355e7b0dcac1
vilmarmoraes <vilmarpomifrai@hotmail.com>
2022-08-05 16:02:25 -0300

    ClientReposity, ClientDTO, ClientService, ClientResource com findAll

---
+++ backend/src/main/java/com/vssolution/DsClient/service/ClientService.java
@@ -0,0 +1,26 @@
+package com.vssolution.DsClient.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.vssolution.DsClient.dto.ClientDTO;
+import com.vssolution.DsClient.entities.Client;
+import com.vssolution.DsClient.repositories.ClientRepository;
+
+@Service
+public class ClientService {
+
+	@Autowired
+	private ClientRepository repository;
+
+	@Transactional(readOnly = true)
+	public Page<ClientDTO> findAllPaged(PageRequest pageRequest) {
+		Page<Client> list = repository.findAll(pageRequest);
+		return list.map(x -> new ClientDTO(x));
+	}
+	
+	
+}
